6497f98420c81074c2758c39d79031d574be41d5
abrakhan916 <abrar.khan916@gmail.com>
2020-02-03 17:41:31 +0000

    Getting cards to appear in html

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+const firstCardButton = document.getElementById('firstCardButton');
+const yourCardImg = document.getElementById('yourCardImg')
+
 class TopTrumps {
-    constructor(name, pace) {
+    constructor(id, name, pace) {
+        this.id = id
         this.name = name;
         this.pace = pace;
+    }
+
+    id() {
+        return this.id
     }
 
     name() {
@@ -13,16 +21,16 @@ class TopTrumps {
 }
 
 const fullDeck = [
-    ronaldo = new TopTrumps("Cristiano Ronaldo", 90),
-    messi = new TopTrumps("Lionel Messi", 87),
-    rashford = new TopTrumps("Marcus Rashford", 92),
-    martial = new TopTrumps("Anthony Martial", 89),
-    dJames = new TopTrumps("Daniel James", 94),
-    mata = new TopTrumps("Juan Mata", 62),
-    matic = new TopTrumps("Nemanja Matic", 48),
-    bFernandes = new TopTrumps("Martial", 77),
-    greenwood = new TopTrumps("Mason Greenwood", 76),
-    lukaku = new TopTrumps("Romelu Lukaku", 75)
+    ronaldo = new TopTrumps("ronaldo", "Cristiano Ronaldo", 90),  //card1
+    messi = new TopTrumps("messi", "Lionel Messi", 87), //card2
+    rashford = new TopTrumps("rashford", "Marcus Rashford", 92), //card3
+    neymar = new TopTrumps("neymar", "Neymar Jr", 91), //card4
+    pogba = new TopTrumps("pogba", "Paul Pogba", 74), //card5
+    mbappe = new TopTrumps("mbappe", "Mbappe", 96), //card6
+    vanDijk = new TopTrumps("vanDijk", "Van Dijk", 77), //card7
+    moSalah = new TopTrumps("moSalah", "Mo Salah", 93), //card8
+    dybala = new TopTrumps("dybala", "Dybala", 83), //card9
+    dJames = new TopTrumps("dJames", "Daniel James", 94), //card10
 ]
 
 let arrayShuffle = function(arr) {
@@ -39,27 +47,49 @@ let arrayShuffle = function(arr) {
 }
 
 let shuffledDeck = arrayShuffle(fullDeck);
-console.log(shuffledDeck);
+// console.log(shuffledDeck);
 
 const deckOne = shuffledDeck.splice(0, 5);
 const deckTwo = shuffledDeck
-console.log("I am the first deck");
-console.log(deckOne)
-console.log("I am the second deck");
-console.log(deckTwo)
+// console.log("I am the first deck");
+// console.log(deckOne)
+// console.log("I am the second deck");
+// console.log(deckTwo)
 
 const compareDecks = () => {
     if (deckOne[0].pace < deckTwo[0].pace) {
         let firstElement = deckOne.shift();
-        deckTwo.push(firstElement)
+        let tempDeck = deckTwo.shift;
+        deckTwo.push(firstElement);
+        deckTwo.push(tempDeck);
     } else {
         let firstElement = deckTwo.shift();
-        deckOne.push(firstElement)
+        let tempDeck = deckOne.shift();
+        deckOne.push(firstElement);
+        deckOne.push(tempDeck);
     }
 }
 
-console.log(compareDecks());
-console.log("deck after comparison")
 console.log(deckOne);
 console.log(deckTwo);
+compareDecks();
+console.log("deck after comparison")
+console.log(deckOne.length)
+console.log(deckTwo.length)
+console.log(deckOne);
+console.log(deckTwo);
+// compareDecks();
+// console.log("deck after comparison")
+// console.log(deckOne.length)
+// console.log(deckTwo.length)
+// console.log(deckOne);
+// console.log(deckTwo);
+
+firstCardButton.addEventListener("click", () =>{
+    compareDecks()
+
+    yourCardImg.src = `images/${deckOne[0].id}.png`;
+    computerCardImg.src = `images/${deckTwo[0].id}.png`;
+    console.log(`images/${deckOne[0].id}.png`)
+})
 
